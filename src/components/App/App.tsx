5375e13b7f06d9d5d1f4d42c561a0ea1f1f6233e
DJ-Chemik <dj-chemik@wp.pl>
2022-02-07 21:09:16 +0100

    Add initial modifications

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactLogo from '../UI/ReactLogo';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ReactLogo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UI/ReactLogo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+
+const ReactLogo = () => {
+  return <img src={logo} className="App-logo" alt="logo" />;
+};
+
+export default ReactLogo;
